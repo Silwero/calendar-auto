f6407aed338b7997e8b186178dacd8c0799e4bbc
OksanaKhorushko <horushkosana@gmail.com>
2023-02-22 14:40:45 +0200

    Add react router

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { App } from '../App';
+import { Login } from '../pages/Login/Login';
+import { Register } from '../pages/Register/Register';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+    ],
+  },
+]);
