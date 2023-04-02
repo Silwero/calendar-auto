40644002be909c6764c5802653bfc0d2888cf3d0
OksanaKhorushko <horushkosana@gmail.com>
2023-04-02 14:39:16 +0300

    add vehicle functionality

--- src/component/AddVehicleModal/AddVehicleModal.jsx
+++ src/component/AddVehicleModal/AddVehicleModal.jsx
@@ -1,11 +1,19 @@
 import { Button, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 import { Modal } from '../base/Modal/Modal';
+import { useState } from 'react';
 
 export const AddVehicleModal = ({ open, handleClose, handleSubmit }) => {
+  const [vehicleName, setVehicleName] = useState('');
+
   const onSubmit = event => {
     event.preventDefault();
 
-    handleSubmit();
+    handleSubmit(vehicleName);
+    setVehicleName('');
+  };
+
+  const handleVehicleNameChange = ({ target }) => {
+    setVehicleName(target.value);
   };
 
   return (
@@ -20,6 +28,8 @@ export const AddVehicleModal = ({ open, handleClose, handleSubmit }) => {
             label="Vehicle Name"
             fullWidth
             variant="standard"
+            value={vehicleName}
+            onChange={handleVehicleNameChange}
           />
         </DialogContent>
         <DialogActions>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,18 +13,42 @@ import { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { BottomActions } from '../../component/BottomActions/BottomActions';
 import { AddVehicleModal } from '../../component/AddVehicleModal/AddVehicleModal';
+import { nanoid } from 'nanoid';
 
 export const HomePage = () => {
   const [isAddVehicleModalOpen, setIsAddVehicleModalOpen] = useState(false);
+  const [vehicleList, setVehicleList] = useState([
+    {
+      id: 1,
+      name: 'Tucson',
+    },
+    {
+      id: 2,
+      name: 'Car',
+    },
+  ]);
+
   const handleAddVehicleClick = () => {
     setIsAddVehicleModalOpen(true);
   };
+
   const handleAddVehicleModalClose = () => {
     setIsAddVehicleModalOpen(false);
   };
 
-  const saveNewVehicle = () => {
-    console.log('save');
+  const saveNewVehicle = name => {
+    setVehicleList([...vehicleList, {
+      id: nanoid(),
+      name,
+    }]);
+
+    setIsAddVehicleModalOpen(false);
+  };
+
+  const deleteVehicle = vehicleId => {
+    const newVehicleList = vehicleList.filter(vehicle => vehicleId !== vehicle.id);
+
+    setVehicleList(newVehicleList);
   };
 
   return (
@@ -33,30 +57,23 @@ export const HomePage = () => {
         <Typography component="h1" variant="h1">My cars</Typography>
         <Paper>
           <List>
-            <ListItem
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton dense>
-                <ListItemText primary="Tushkan" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem
-              secondaryAction={
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton dense>
-                <ListItemText primary="Car" />
-              </ListItemButton>
-            </ListItem>
+            {vehicleList.map(vehicle => {
+              return (
+                <ListItem
+                  key={vehicle.id}
+                  secondaryAction={
+                    <IconButton onClick={() => deleteVehicle(vehicle.id)} edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                  disablePadding
+                >
+                  <ListItemButton dense>
+                    <ListItemText primary={vehicle.name} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
           </List>
         </Paper>
       </Container>
