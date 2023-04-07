9c12fd1c534099370ca776dce2c532347c08b721
OksanaKhorushko <horushkosana@gmail.com>
2023-04-07 17:01:32 +0300

    vehicle edit modal

---
+++ src/component/EditVehicleModal/EditVehicleModal.jsx
@@ -0,0 +1,42 @@
+import { Button, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { Modal } from '../base/Modal/Modal';
+import { useState } from 'react';
+
+export const EditVehicleModal = ({ open, handleClose, handleSubmit }) => {
+  const [vehicleName, setVehicleName] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    handleSubmit(vehicleName);
+    setVehicleName('');
+  };
+
+  const handleVehicleNameChange = ({ target }) => {
+    setVehicleName(target.value);
+  };
+
+  return (
+    <Modal open={open} handleClose={handleClose}>
+      <DialogTitle>Edit vehicle</DialogTitle>
+      <form onSubmit={onSubmit}>
+        <DialogContent>
+          <TextField
+            required
+            autoFocus
+            margin="dense"
+            label="Vehicle Name"
+            fullWidth
+            variant="standard"
+            value={vehicleName}
+            onChange={handleVehicleNameChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="error">Cancel</Button>
+          <Button type="submit">Save</Button>
+        </DialogActions>
+      </form>
+    </Modal>
+  );
+};
--- src/component/VehicleList/VehicleList.jsx
+++ src/component/VehicleList/VehicleList.jsx
@@ -1,11 +1,16 @@
 import { List } from '@mui/material';
 import { VehicleListItem } from './VehicleListItem';
 
-export const VehicleList = ({ vehicleList, deleteVehicle }) => {
+export const VehicleList = ({ vehicleList, deleteVehicle, handleEditVehicleClick }) => {
   return (
     <List>
       {vehicleList.map(({ name, id }) => {
-        return <VehicleListItem name={name} key={id} handleDelete={() => deleteVehicle(id)} />;
+        return <VehicleListItem
+          name={name}
+          key={id}
+          handleDelete={() => deleteVehicle(id)}
+          handleEditVehicleClick={() => handleEditVehicleClick(id)}
+        />;
       })}
     </List>
   );
